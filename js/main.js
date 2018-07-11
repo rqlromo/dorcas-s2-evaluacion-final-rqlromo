f670cab1c1ae7f2fd41c6b4c34e1cd629f254d30
Raquel Romo <Raquel@MacBook-Pro-de-Eduardo.local>
2018-07-11 20:58:38 +0200

    final search and refresh

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var inputSerie = document.querySelector('.input-serie');
 var searchSerie = document.querySelector('.search-serie');
 var body = document.querySelector('body');
+var parentBox = document.createElement('div');
 
 function getSerie(){
   var serie = inputSerie.value;
@@ -11,8 +12,10 @@ function getSerie(){
   var newContentSerieTitle;
   var tagImageSerie;
   var imageNoExist;
-  //var fillImage;
+  var fillImage;
   var imageSerie;
+  var tagBoxSeries;
+  parentBox.innerHTML = ' ';
   //console.log(inputSerie);
   //console.log('valor de input serie:', serie);
   //console.log('http://api.tvmaze.com/search/shows?q=' + serie);
@@ -22,12 +25,28 @@ function getSerie(){
     })
     .then(function(buscardorSeriesJSON){
       //console.log('json api series:',buscardorSeriesJSON);
-      console.log('tamaño json api series:',buscardorSeriesJSON.length);
+      //console.log('tamaño json api series:',buscardorSeriesJSON.length);
       //console.log('series de json api series:',buscardorSeriesJSON[0].show);
       //console.log('series de json api series:',buscardorSeriesJSON[0].show.name);
       //console.log('series de json api series:',buscardorSeriesJSON[0].show.image);
       //console.log('series de json api series:',buscardorSeriesJSON[0].show.image.original);
       for(var i = 0; i < buscardorSeriesJSON.length; i++){
+        tagBoxSeries = document.createElement('div');
+        tagImageSerie = document.createElement('img');
+        //console.log('muestrame todas las url de las imagenes:',buscardorSeriesJSON[i].show.image);
+        imageNoExist = buscardorSeriesJSON[i].show.image;
+        if (imageNoExist === null ){
+          //console.log('entro aqui en algun momento?');
+          fillImage = 'https://via.placeholder.com/210x295/ccacccc/6aaaa6/?text=:(';
+          tagImageSerie.src = fillImage;
+        } else {
+          //console.log('me vuelvo loca:', buscardorSeriesJSON[i].show.image);
+          imageSerie = buscardorSeriesJSON[i].show.image.medium;
+          //console.log('tienes url de imagen?',imageSerie);
+          //Cremaos nuestra etiqueta imagen
+          //insertamos imagen en body
+          tagImageSerie.src = imageSerie;
+        }
         //pasamos el bucle for por cada elemento del Json y recogemos el titulo
         tileSerie = buscardorSeriesJSON[i].show.name;
         //Cremaos nuestra etiqueta parrafo
@@ -38,25 +57,19 @@ function getSerie(){
         body.appendChild(tagTitleSerie);
         //insertamos el titulo en el parrafo
         tagTitleSerie.appendChild(newContentSerieTitle);
-        tagImageSerie = document.createElement('img');
-        console.log('muestrame todas las url de las imagenes:',buscardorSeriesJSON[i].show.image);
-        imageNoExist = buscardorSeriesJSON[i].show.image;
-        if (imageNoExist === null ){
-          console.log('entro aqui en algun momento?');
-          tagImageSerie.src = 'https://via.placeholder.com/210x295/cccccc/666666/?text=TV';
-        } else {
-          //console.log('me vuelvo loca:', buscardorSeriesJSON[i].show.image);
-          imageSerie = buscardorSeriesJSON[i].show.image.medium;
-          //console.log('tienes url de imagen?',imageSerie);
-          //Cremaos nuestra etiqueta imagen
-          //insertamos imagen en body
-          body.appendChild(tagImageSerie);
-          tagImageSerie.src = imageSerie;
-        }
+        tagBoxSeries.appendChild(tagTitleSerie);
+        tagBoxSeries.appendChild(tagImageSerie);
+        parentBox.appendChild(tagBoxSeries);
+        body.appendChild(parentBox);
+        //.setAttribute('class', 'box-series');
       }
     });
 }
 
+
+// fucntion favourites(){
+
+// }
 
 
 searchSerie.addEventListener('click',getSerie);
